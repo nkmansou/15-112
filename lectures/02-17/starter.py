d35029551c6ae5ddd0a8c934f41987ed44cfb927
ThierrySans <thierry.sans@gmail.com>
2015-02-17 14:04:44 +0300

    new starter

--- lectures/02-17/starter.py
+++ lectures/02-17/starter.py
@@ -1,36 +1,29 @@
-##### Data #####
-
-tab = [[ 456 , 235 , 987 , 673 , 776 , 427 ], [ 856 , 284 , 413 , 987 , 629 , 196 ], [ 198 , 936 , 648 , 925 , 435 , 662 ], [174 , 816 , 367 , 357 , 856 , 1045 ]]
-
-months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
-sellers = ["Fahad", "Emad", "Mariam", "Sarah"]
-
 # Q1: What is Fahad's revenue?
 # Good solution: write a function that takes a seller as argument
 # and returns the revenue for this seller
 
-def sellerRevenue(name):
+def sellerRevenue(record, months, sellers, name):
    return 0
 
 # Q2: Who is the best seller?
 # Good solution: write a function that does not take any argument
 # and returns the name of the best seller
 
-def bestSeller():
+def bestSeller(record, months, sellers):
     return ""
 
 # Q3: What was the revenue in February?
 # Good solution: write a function that takes a month as argument
 # and returns the revenue for this month
 
-def monthRevenue(month):
+def monthRevenue(record, months, sellers, month):
     return 0
 
 # Q4: when was the best month?
 # Good solution: write a function that does not take any argument
 # and returns the name of the best month
 
-def bestMonth():
+def bestMonth(record, months, sellers):
     return ""
 
 # Q5: what is the total revenue?
@@ -38,5 +31,15 @@ def bestMonth():
 # solution 1: iterate through the sellers and add their revenue
 # solution 2: iterate through the months and add their revenue
 
-def total():
+def total(record, months, sellers):
     return 0
+
+
+##### Data #####
+
+tab = [[ 456 , 235 , 987 , 673 , 776 , 427 ], [ 856 , 284 , 413 , 987 , 629 , 196 ], [ 198 , 936 , 648 , 925 , 435 , 662 ], [174 , 816 , 367 , 357 , 856 , 1045 ]]
+
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+sellers = ["Fahad", "Emad", "Mariam", "Sarah"]
+
+print sellerRevenue(tab, months, sellers, "Fahad")
